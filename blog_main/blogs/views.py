0df9fe96211d4dfb43d628eb27fdc5b63feb63a7
SamarSingla0 <samarsingla04@gmail.com>
2025-12-09 16:07:47 +0530

    new commit

--- blog_main/blogs/views.py
+++ blog_main/blogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Blog
 # Create your views here.
+
+def posts_by_category(requset, category_id):
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    context = {
+        'posts':posts,
+    }
+    return render(requset, 'posts_by_category.html', context)
