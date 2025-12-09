947e8c975f4ec9ce7e7b064cd2eb192956ee138d
SamarSingla0 <samarsingla04@gmail.com>
2025-12-09 18:59:23 +0530

    added about us section working tommarow

--- blog_main/blogs/views.py
+++ blog_main/blogs/views.py
@@ -17,3 +17,12 @@ def posts_by_category(requset, category_id):
         'category': category
     }
     return render(requset, 'posts_by_category.html', context)
+
+
+
+def blogs(request, slug):
+    single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    context = {
+        'single_blog': single_blog,
+    }
+    return render(request, 'blogs.html')
