4263f69a1d03b0324a613ddfd8acb8cf692dfac0
SamarSingla0 <samarsingla04@gmail.com>
2025-12-15 12:30:03 +0530

    added new post button with full function handling the same slug problem and provide the unique slug upon same title

--- blog_main/dashboards/views.py
+++ blog_main/dashboards/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
-from .forms import CategoryForm
+from .forms import CategoryForm, BlogPostForm
+from django.template.defaultfilters import slugify
 # Create your views here.
 @login_required(login_url='login')
 def dashboard(request):
@@ -29,7 +30,7 @@ def add_category(request):
             form.save()
             return redirect('categories')
         
-    form = CategoryForm
+    form = CategoryForm()
     context = {
         'form': form,
     }
@@ -58,3 +59,35 @@ def delete_category(request, pk):
     category = get_object_or_404(Category, pk=pk)
     category.delete()
     return redirect('categories')
+
+
+
+def posts(request):
+    posts = Blog.objects.all()
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'dashboard/posts.html', context)
+
+
+
+def add_post(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False) #we are temporarily storing the form so that we can add the author name automaticaly assigned in user section
+            post.author = request.user
+            post.save()
+            title = form.cleaned_data['title']
+            post.slug = slugify(title) + '-' + str(post.id)
+            post.save()
+            return redirect('posts')
+        else:
+            pass
+        
+        
+    form = BlogPostForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/add_post.html', context)
