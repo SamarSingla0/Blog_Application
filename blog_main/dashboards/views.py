a4fcef556580e074207b64bfb50c073637e49e5e
SamarSingla0 <samarsingla04@gmail.com>
2025-12-15 11:24:46 +0530

    added edit and delete category options

--- blog_main/dashboards/views.py
+++ blog_main/dashboards/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
-
+from .forms import CategoryForm
 # Create your views here.
 @login_required(login_url='login')
 def dashboard(request):
@@ -20,3 +20,41 @@ def dashboard(request):
 def categories(request):
     
     return render(request, 'dashboard/categories.html')
+
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+        
+    form = CategoryForm
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/add_category.html', context)
+
+
+
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+        
+    form = CategoryForm(instance=category)
+    context = {
+        'form': form,
+        'category':category,
+    }
+    return render(request, 'dashboard/edit_category.html', context)
+
+
+
+def delete_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    return redirect('categories')
